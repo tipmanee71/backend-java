e23b236d400a1817151694cc7fe8e836b2018429
tipmanee71 <135458090+tipmanee71@users.noreply.github.com>
2023-08-01 11:32:20 +0700

    lab3

--- src/main/java/com/example/demo/controller/Employeecontroller.java
+++ src/main/java/com/example/demo/controller/Employeecontroller.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,71 +16,107 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
 import com.example.demo.model.Product;
+import com.example.demo.repository.EmployeeRepository;
 
 @RestController
-
 public class Employeecontroller {
-	private List<Employee> data = new ArrayList<Employee>();
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+//	private List<Employee> data = new ArrayList<Employee>();
 
 	@GetMapping("employee")
 	public List<Employee> getEmployee() {
-		return data;
+		return employeeRepository.findAll();
 	}
 
 	@PostMapping("/employee") // ยิง method //ถ้ามี post จะมี body
 	public Employee addEmployee(@RequestBody Employee body) {
-		for (int i = 0; i < data.size(); i++) {
-			if (body.getEmployeeid() == data.get(i).getEmployeeid()) { /// หาเพื่อมีหรือไม่แต่ไม่ได้นำไปใช่ต่อ
-				return null;
-			}
-		}
-		data.add(body);
-		return body;
+//		for (int i = 0; i < data.size(); i++) {
+//			if (body.getEmployeeid() == data.get(i).getEmployeeid()) { /// หาเพื่อมีหรือไม่แต่ไม่ได้นำไปใช่ต่อ
+//				return null;
+//			}
+//		}
+		// data.add(body);
+
+		return employeeRepository.save(body);
 	}
 
 	// สามารถส่งไอดีอะไรก็ได้ที่เป็น integer ถ้าส่งชนิดอื่นจะerror
 
 	@GetMapping("/employee/{employeeid}")
-	public Employee getEmployeeDetail(@PathVariable Integer employeeid) {
-		System.out.println("empolyeeid =" + employeeid);
+	public Optional<Employee> getEmployeeDetail(@PathVariable Integer employeeid) {
+//		System.out.println("empolyeeid =" + employeeid);
+//
+//		for (int i = 0; i < data.size(); i++) {
+//			if (employeeid == data.get(i).getEmployeeid()) { // หาว่ามีและนำไปใช้ต่อ
+//				return data.get(i);
+//			}
+//		}
 
-		for (int i = 0; i < data.size(); i++) {
-			if (employeeid == data.get(i).getEmployeeid()) { // หาว่ามีและนำไปใช้ต่อ
-				return data.get(i);
-			}
-		}
-		return null;
+		Optional<Employee> employee = employeeRepository.findById(employeeid);
+
+		return employee;
 	}
 
 	@PutMapping("employee/{employeeid}")
 	public Employee updateEmployee(@PathVariable Integer employeeid, @RequestBody Employee body) {
 
-		for (int i = 0; i < data.size(); i++) {
-			if (employeeid == data.get(i).getEmployeeid()) {
+//		for (int i = 0; i < data.size(); i++) {
+//			if (employeeid == data.get(i).getEmployeeid()) {
+//
+//				data.get(i).setFirstName(body.getFirstName());
+//				data.get(i).setLastName(body.getLastName());
+//				data.get(i).setSalary(body.getSalary());
+//
+//				return data.get(i);
+//			}
+//		}
 
-				data.get(i).setFirstName(body.getFirstName());
-				data.get(i).setLastName(body.getLastName());
-				data.get(i).setSalary(body.getSalary());
+		Optional<Employee> employee = employeeRepository.findById(employeeid);
 
-				return data.get(i);
-			}
+		if (employee.isPresent()) {
+			// or Employee employeeEdit =	employee.get();
+			employee.get().setFirstName(body.getFirstName());   // or //employeeEdit.setFirstName(body.getFirstName()); 
+			employee.get().setLastName(body.getLastName());		// or //employeeEdit.setLastName(body.getLastName());	
+			employee.get().setSalary(body.getSalary());			// or //employeeEdit.setSalary(body.getSalary());	
+			employee.get().setEmployeeid(body.getEmployeeid());	// or //employeeEdit.setEmployeeid(body.getEmployeeid());
+
+			employeeRepository.save(employee.get()); //or //	employeeRepository.save(employeeEdit);
+			return employee.get();
+
+		} else {
+			return null;
 		}
-		return null;
+
 	}
 
 	@DeleteMapping("employee/{employeeid}")
 	public String deleteString(@PathVariable Integer employeeid, @RequestBody Employee body) {
 
-		for (int i = 0; i < data.size(); i++) {
-			if (employeeid == data.get(i).getEmployeeid()) {
-				data.remove(i);
+//		for (int i = 0; i < data.size(); i++) {
+//			if (employeeid == data.get(i).getEmployeeid()) {
+//				data.remove(i);
+//				return "delete sucsess;";
+//
+//			}
+//		}
+//
+//		return "employee not found";
+	
+			Optional<Employee> employee = employeeRepository.findById(employeeid);		
+		
+			if ( employee.isPresent()) {
+				employeeRepository.delete(employee.get());
+				
 				return "delete sucsess;";
-
+				
+			}else {
+				return "employee not found";
 			}
+		
 		}
-
-		return "employee not found";
-	}
 
 
 }
--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,15 +1,38 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
-	private Integer employeeid; //private เรียกใช้ได้แค่ภายใน class   
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private Integer employeeid; // private เรียกใช้ได้แค่ภายใน class
 	private String firstName;
 	private String lastName;
 	private Integer salary;
+
 	
 	
+
 	public Employee() {
 		super();
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
 	public Employee(Integer employeeid, String firstName, String lastName, Integer salary) {
 		super();
 		this.employeeid = employeeid;
@@ -17,31 +40,38 @@ public class Employee {
 		this.lastName = lastName;
 		this.salary = salary;
 	}
+
+
 	public Integer getEmployeeid() {
 		return employeeid;
 	}
-	public void setEmployee(Integer employeeid) {
+
+	public void setEmployeeid(Integer employeeid) {
 		this.employeeid = employeeid;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public Integer getSalary() {
 		return salary;
 	}
+
 	public void setSalary(Integer salary) {
 		this.salary = salary;
 	}
-	
-	
 
 }
